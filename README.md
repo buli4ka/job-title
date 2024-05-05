d01122f136eb9dc632f555c8f738697aba8a1e07
Andrii <Andrii.Opalko@nordstrom.com>
2024-05-06 00:43:05 +0300

    added readme, removed weather forecast

---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# ReadME
+
+### Job Title Asp.Net Core project with only one entity with Id and Name fields
+### Used DDD Architecture. EF Core Framework with SQLite for DB configuration. 
--- src/Api/Controllers/JobTitle/JobTitleController.cs
+++ src/Api/Controllers/JobTitle/JobTitleController.cs
@@ -8,38 +8,35 @@ namespace Api.Controllers.JobTitle;
 [Route("[controller]")]
 public class JobTitleController(IJobTitleService jobTitleService) : ControllerBase
 {
-    private readonly IJobTitleService _jobTitleService = jobTitleService;
-
-
     [HttpGet("getJobTitles")]
     public Task<IEnumerable<JobTitleDto>> GetAll()
     {
-        return _jobTitleService.GetAllJobTitles();
+        return jobTitleService.GetAllJobTitles();
     }  
     
     [HttpGet("getJobTitle/{jobTitleId:guid}")]
     public Task<JobTitleDto> GetById(Guid jobTitleId)
     {
-        return _jobTitleService.GetJobTitleById(jobTitleId);
+        return jobTitleService.GetJobTitleById(jobTitleId);
     }
 
     [HttpPost("createJobTitle")]
     public Task<JobTitleDto> CreateJobTitle(CreateJobTitleDto jobTitleDtoDto)
     {
-        return _jobTitleService.CreateJobTitle(jobTitleDtoDto);
+        return jobTitleService.CreateJobTitle(jobTitleDtoDto);
     }
     
     [HttpPut("updateJobTitle")]
     public async Task<IActionResult> UpdateJobTitle(JobTitleDto jobTitleDto)
     {
-        await _jobTitleService.UpdateJobTitle(jobTitleDto);
+        await jobTitleService.UpdateJobTitle(jobTitleDto);
 
         return Ok();
     }    
     [HttpDelete("deleteJobTitle/{jobTitleId:guid}")]
     public async Task<IActionResult> DeleteJobTitle(Guid jobTitleId)
     {
-        await _jobTitleService.DeleteJobTitleById(jobTitleId);
+        await jobTitleService.DeleteJobTitleById(jobTitleId);
 
         return NoContent();
     }
